a9f619ee90c5071d0e137a76ad4b7791e3bb886f
Kazunori Jo <itochan315@gmail.com>
2017-08-01 02:28:47 +0900

    Make progressbar

--- m2tsview.rb
+++ m2tsview.rb
@@ -1,5 +1,6 @@
 require 'pp'
 require 'optparse'
+require 'ruby-progressbar'
 
 option = {}
 opts = OptionParser.new
@@ -18,26 +19,35 @@ mask = [
   Mask.new('Continuity counter', 0xf, 0)
 ]
 
-stats = {
-  pid_count: {}
-}
+pid_count = {}
+
 File.open(ARGV[0]) do |f|
+  size = f.size
+  progressbar = ProgressBar.create(
+    title: 'Analyze',
+    total: size / 188,
+    format: '%t: |%B| %P% %E'
+  )
+
   until f.eof?
     division = f.read(4).bytes.reverse.map.with_index { |n, i| n << i * 8 }.sum
 
     mask.each do |m|
-      value = ((division & m.bit << m.shift) >> m.shift).to_s(16)
-      puts "#{m.description}: 0x#{value}" if option[:verbose]
+      value = "0x#{((division & m.bit << m.shift) >> m.shift).to_s(16)}"
+      puts "#{m.description}: #{value}" if option[:verbose]
 
       if m.description == 'PID'
-        stats[:pid_count][value] = 0 unless stats[:pid_count][value]
-        stats[:pid_count][value] += 1
+        pid_count[value] = 0 unless pid_count[value]
+        pid_count[value] += 1
       end
     end
-    puts
+    puts if option[:verbose]
 
     f.seek(184, IO::SEEK_CUR)
+
+    progressbar.increment
   end
 end
 
-pp stats
+puts 'PID Count'
+pid_count.sort.each { |p| puts "#{p[0]}: #{p[1]}" }
